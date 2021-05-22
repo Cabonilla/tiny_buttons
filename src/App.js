c291a951fdaed31e5c7fc19796c61727359547f5
cabonilla <christian.bonilla01@gmail.com>
2021-05-22 13:03:08 -0700

    Basic layout done.

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="buttonSquare">Hello, Lave</div>
-      <div className="buttonSquare">Hello, Lave</div>
-      <div className="buttonSquare">Hello, Lave</div>
-      <div className="buttonSquare">Hello, Lave</div>
-      <div className="buttonSquare">Hello, Lave</div>
-      <div className="buttonSquare">Hello, Lave</div>
-      <div className="buttonSquare">Hello, Lave</div>
-      <div className="buttonSquare">Hello, Lave</div>
-      <div className="buttonSquare">Hello, Lave</div>
+      <div className="buttonSquare">Hello, Laverne</div>
+      <div className="buttonSquare">Hello, Laverne</div>
+      <div className="buttonSquare">Hello, Laverne</div>
+      <div className="buttonSquare">Hello, Laverne</div>
+      <div className="buttonSquare">Hello, Laverne</div>
+      <div className="buttonSquare">Hello, Laverne</div>
+      <div className="buttonSquare">Hello, Laverne</div>
+      <div className="buttonSquare">Hello, Laverne</div>
+      <div className="buttonSquare">Hello, Laverne</div>
     </div>
   );
 }
