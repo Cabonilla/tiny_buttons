a3ae3eaed8e04b37c72fb76b8ccdb130bb2709a3
cabonilla <christian.bonilla01@gmail.com>
2021-05-22 12:40:45 -0700

    Added a README.

---
+++ src/components/NeonButton.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './NeonButton.module.css'
+
+const neonButton = () => {
+  return <button className={styles.button}>Hello, Laverne</button>
+}
+
+export default neonButton
